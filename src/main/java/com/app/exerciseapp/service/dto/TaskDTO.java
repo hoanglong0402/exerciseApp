39c9b7628d0a900379bc67ba3ab4ceefc8f9b642
LV <hoanglong4296@gmail.com>
2024-11-24 20:12:50 +0700

    adding validate task

--- src/main/java/com/app/exerciseapp/service/dto/TaskDTO.java
+++ src/main/java/com/app/exerciseapp/service/dto/TaskDTO.java
@@ -1,7 +1,12 @@
 package com.app.exerciseapp.service.dto;
 
 import com.app.exerciseapp.domain.enumeration.TaskStatus;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -14,7 +19,14 @@ public class TaskDTO {
 
     private Long assignedTo;
     private String assignedUserName;
+
+    @NotNull
+    @Size(min = 5, max = 50)
     private String title;
+
+    @Size(min = 5, max = 200)
     private String description;
+
+    @JsonSetter(nulls = Nulls.SKIP)
     private TaskStatus status = TaskStatus.NEW;
 }
